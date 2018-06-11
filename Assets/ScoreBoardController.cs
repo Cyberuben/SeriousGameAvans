f9cd314b2d64f3ec9da8256a3e5fef0cbd96b1dd
zaveion <anthonypeeters221@hotmail.com>
2018-06-11 16:15:00 +0200

    added scoreboard

---
+++ Assets/ScoreBoardController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoardController : MonoBehaviour {
+
+    public Text rowExample;
+
+    public User[] users;
+	// Use this for initialization
+
+	void Start () {
+        users = new User[] { new User("Test user", 100), new User("test user 2",200), new User("test user 3", 200), new User("test user 4", 200), new User("Test user", 100), new User("test user 2", 200), new User("test user 3", 200), new User("test user 4", 200), new User("Test user", 100), new User("test user 2", 200) };
+        //TODO: get top 10 rows	from api and deserialize
+
+        Camera camera = Camera.main;
+        //float heightOneRow = transform.
+        Canvas canvas = GetComponent<Canvas>();
+        for (int i = 0; i < users.Length; i++)
+        {
+            Text text = rowExample;
+            Text new_text = Instantiate(text, new Vector3(0,  0), Quaternion.identity);
+            new_text.text = "Gebruiker:" + users[i].UserName + "        Highscore:" + users[i].HighScore;
+            new_text.transform.SetParent(transform);
+            Debug.Log(canvas.pixelRect);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour {
+
+    public string UserName { get; set; }
+    public int HighScore { get; set; }
+
+    public User(string userName, int highScore)
+    {
+        UserName = userName;
+        HighScore = highScore;
+    }
+
+    public User() { }
+}
