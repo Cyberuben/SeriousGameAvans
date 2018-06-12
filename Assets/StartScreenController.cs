ea8b9407c2166908f7f2c2aa8e5ac14eed010cb4
zaveion <anthonypeeters221@hotmail.com>
2018-06-12 12:15:55 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/StartScreenController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScreenController : MonoBehaviour
+{
+
+    public Text labelToEdit;
+    private string startupText;
+
+    // Use this for initialization
+    void Start()
+    {
+        startupText = "WAAR ONDER- EN BOVENWERELD ELKAAR ONTMOETEN";
+        StartCoroutine(DrawText());
+    }
+
+    IEnumerator DrawText()
+    {
+        for (int i = 0; i < startupText.Length; i++)
+        {
+            labelToEdit.text += startupText[i];
+            yield return new WaitForSeconds(0.08f);
+        }
+    }
+}
